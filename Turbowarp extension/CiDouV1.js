8e516440297fd01e359d103dce08f31422db3e30
LittleSamoyed <122708551+LittleSamoyed@users.noreply.github.com>
2023-01-21 16:03:10 +0800

    1
    1

--- Turbowarp extension/CiDouV1.js
+++ Turbowarp extension/CiDouV1.js
@@ -20,7 +20,7 @@ class StrictEqualityExtension {
     }
     strictlyEquals(args) {
       // Note strict equality: Inputs must match exactly: in type, case, etc.
-      window.open(ONE);
+      window.open(args.ONE);
     }
   }
   Scratch.extensions.register(new StrictEqualityExtension());

1fa9b6dd404b1467b037138300b9c8bfe4d66523
LittleSamoyed <122708551+LittleSamoyed@users.noreply.github.com>
2023-01-21 07:20:00 +0000

    CiDou extension v1

---
+++ Turbowarp extension/CiDouV1.js
@@ -0,0 +1,26 @@
+class StrictEqualityExtension {
+    getInfo() {
+      return {
+        id: 'cidouv1', // id
+        name: 'CiDou Block', //扩展名称 
+        blocks: [
+          {
+            opcode: 'strictlyEquals',
+            blockType: Scratch.BlockType.COMMAND, //类型
+            text: 'Open[ONE]',
+            arguments: {
+              ONE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'https://www.kaka.city/'
+              }
+            }
+          }
+        ]
+      };
+    }
+    strictlyEquals(args) {
+      // Note strict equality: Inputs must match exactly: in type, case, etc.
+      window.open(args.ONE);
+    }
+  }
+  Scratch.extensions.register(new StrictEqualityExtension());
